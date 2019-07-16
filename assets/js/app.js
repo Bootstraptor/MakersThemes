98d893e89e726091a0cd93818359b42cf55ca1da
Frontenda <bootstraptor@gmail.com>
2019-07-16 17:19:15 +0300

    disactivate gsheet widget
    disactivate gsheet widget

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,44 +56,44 @@ $('.slider-testimonials-1').slick({
     }
   });
 
-  var googleSheetUrl="https://docs.google.com/spreadsheets/d/1OWQqEZlWMOof_czquJYtMbFJSFg-UBsnVQ0OMeq23hg/edit?usp=sharing"
+  //var googleSheetUrl="https://docs.google.com/spreadsheets/d/1OWQqEZlWMOof_czquJYtMbFJSFg-UBsnVQ0OMeq23hg/edit?usp=sharing"
   //Send request to G-sheets if ok => start widget
-  var spreadsheetID = new RegExp("/spreadsheets/d/([a-zA-Z0-9-_]+)").exec(googleSheetUrl)[1];
+//  var spreadsheetID = new RegExp("/spreadsheets/d/([a-zA-Z0-9-_]+)").exec(googleSheetUrl)[1];
   // Make sure it is Public and Published to Web or set to Anyone with link can view
-  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+//  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
 
   //var dataL = [];
-  var dataL;
-  var count;
-  var dnld = 2000;
-  var xhr2 = new XMLHttpRequest();
+//  var dataL;
+//  var count;
+//  var dnld = 2000;
+//  var xhr2 = new XMLHttpRequest();
   //Call the open function, GET-type of request, url, true-asynchronous
-  xhr2.open("GET", url, true);
-  xhr2.onerror = function () {
-      console.log('WIDGET ERROR: ** An error occurred during the transaction: Google Sheet file does not exist or wrong ID! Check that file exist and published in web or check ID. See more info on https://proven.ly/documentation')
-  };
+//  xhr2.open("GET", url, true);
+//  xhr2.onerror = function () {
+    //  console.log('WIDGET ERROR: ** An error occurred during the transaction: Google Sheet file does not exist or wrong ID! Check that file exist and published in web or check ID. See more info on https://proven.ly/documentation')
+//  };
   //call the onload
-  xhr2.onload = function() {
+//  xhr2.onload = function() {
     //check if the status is 200(means everything is okay)
-    if (this.readyState == 4 && this.status === 200 && this.status < 400) {
+  //  if (this.readyState == 4 && this.status === 200 && this.status < 400) {
       //return server response as an object with JSON.parse
       //console.log(JSON.parse(this.responseText));
-      dataL = JSON.parse(this.responseText).feed.entry.length;
-      count = +dataL + dnld
-      console.log(count);
-      console.log('B I N G O!');
-      startWidget();
+  //    dataL = JSON.parse(this.responseText).feed.entry.length;
+  //    count = +dataL + dnld
+  //    console.log(count);
+  //    console.log('B I N G O!');
+  //    startWidget();
 
-    }
+  //  }
 
-  };
+  //};
   //call send
-  xhr2.send();
+  //xhr2.send();
 
 
 
 
-  console.log(count)
+  //console.log(count)
   //var content = document.querySelector(".random-wrapper");
   //var content = document.querySelector(".inline-proof");
 
@@ -114,5 +114,7 @@ $('.slider-testimonials-1').slick({
       }, stepTime);
     }
 
-    animateValue("value", 1, count, 100);
+    animateValue("value", 1, 2630, 100);
   }
+
+  startWidget();
