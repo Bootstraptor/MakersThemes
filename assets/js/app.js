74b43cfb9b3fd7728cdcc010d40386359c528c37
Frontenda <bootstraptor@gmail.com>
2019-07-16 19:52:27 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,6 @@
-
-
-//Animate on Scroll
 AOS.init({
   duration: 1200,
 })
-
-
-// SlIK SLIDER
-
-//Slik slider init
 $('.slider-testimonials-1').slick({
    slidesToShow: 1,
    slidesToScroll: 1,
@@ -32,36 +24,26 @@ $('.slider-testimonials-1').slick({
     nav.removeClass('shadow-sm');
     nav.addClass('bg-transparent');
   })
-
   $(window).scroll(function() {
-
     var top = 60;
-
     if ($(window).scrollTop() >= top) {
-
         nav.addClass('bg-light');
         nav.removeClass('bg-transparent');
         nav.addClass('shadow-sm');
-
     } else {
-
         nav.removeClass('bg-light');
         nav.removeClass('shadow-sm');
         nav.addClass('bg-transparent');
         if ($(collapseNav).hasClass('show')){
           $(navBtn).trigger('click')
         }
-
-
     }
   });
-
   var googleSheetUrl="https://docs.google.com/spreadsheets/d/1OWQqEZlWMOof_czquJYtMbFJSFg-UBsnVQ0OMeq23hg/edit?usp=sharing"
   //Send request to G-sheets if ok => start widget
   var spreadsheetID = new RegExp("/spreadsheets/d/([a-zA-Z0-9-_]+)").exec(googleSheetUrl)[1];
   // Make sure it is Public and Published to Web or set to Anyone with link can view
   var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
-
   //var dataL = [];
   var dataL;
   var count;
@@ -80,25 +62,11 @@ $('.slider-testimonials-1').slick({
       //console.log(JSON.parse(this.responseText));
       dataL = JSON.parse(this.responseText).feed.entry.length;
       count = +dataL + dnld
-      console.log(count);
-      console.log('B I N G O!');
       startWidget();
-
     }
-
   };
-  //call send
   xhr2.send();
-
-
-
-
-  console.log(count)
-  //var content = document.querySelector(".random-wrapper");
-  //var content = document.querySelector(".inline-proof");
-
    function startWidget(){
-
     function animateValue(id, start, end, duration) {
       var range = end - start;
       var current = start;
@@ -113,6 +81,5 @@ $('.slider-testimonials-1').slick({
           }
       }, stepTime);
     }
-
     animateValue("value", 1, count, 100);
   }
